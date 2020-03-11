e6b6b59936465926edd757d3919765d99b310cc1
Winston 盧文松 <aa71435723@gmail.com>
2020-03-11 23:24:42 +0800

    init

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module eggplant-test-case
+module eggplant-jenkins
 
 go 1.12
-
-require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	h           bool
+	apiScheme   string
+	apiHost     string
+	apiPort     string
+	apiUser     string
+	apiPassword string
+)
+
+func init() {
+	flag.BoolVar(&h, "h", false, "this help")
+	flag.StringVar(&apiScheme, "apiScheme", "http", "http or https (depending on server configuration, http is default)")
+	flag.StringVar(&apiHost, "apiHost", "127.0.0.1", "IP address of ePM server")
+	flag.StringVar(&apiPort, "apiPort", "8080", "Port of ePM server")
+	flag.StringVar(&apiUser, "apiUser", "admin", "ePM user username")
+	flag.StringVar(&apiPassword, "apiPassword", "admin", "ePM user password")
+
+	// 改变默认的 Usage，flag包中的Usage 其实是一个函数类型。这里是覆盖默认函数实现，具体见后面Usage部分的分析
+	// flag.Usage = usage
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	flag.Parse()
+
+	if h {
+		flag.Usage()
+	}
+}
+
+func ExecuteEggplantAPI() {
+	baseUri := fmt.Sprintf("%s://%s:%s", apiScheme, apiHost, apiPort)
+	fmt.Println(baseUri)
+
+	apiUriPrefix := "api"
+	testResourceName := "test"
+	testRunResourceName := "test_run"
+	configurationResourceName := "execution_configuration"
+	execute := "execute"
+	executePollDuration := 5
 }
